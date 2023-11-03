4fa1e01d8aefd961019577da140c46c7f4a6b7b0
Lee Chirinos <90184155+leeChirinos@users.noreply.github.com>
2023-11-03 12:43:29 -0300

    Delete demo directory

--- demo/src/main/java/servicio/ISocioservicio.java
+++
@@ -1,9 +0,0 @@
-package servicio;
-import com.example.demo.Entity.Socio;
-import java.util.List;
-public interface ISocioservicio {
-    public default List<Socio> ListarTotaldeSocios() {
-        return null;
-    }
-
-}
